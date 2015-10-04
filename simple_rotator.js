9372b54507eac46b9b18a070007c32fd931e40ab
Theodoros Ploumis <me@theodorosploumis.com>
2015-10-04 19:26:37 +0300

    Update simple_rotator.js

--- simple_rotator.js
+++ simple_rotator.js
@@ -1,15 +1,13 @@
 // Simple Rotator generic function
 (function($) {
 $.fn.simpleRotator = function(options) {
-  // Default settings
+
   var settings = $.extend({
-    // A random unique ID for this rotator. This will be added to the classes later.
-    unique_id: 'myid',
-    // Items (childen of the wrapepr) to be rotated.
+    // These are the defaults.
+    unique_id: 'new123',
     item_class: ".item",
-    // How many items to be visible each time
     items_visible: 3,
-  }, options);
+  }, options );
 
   var item_wrapper = this;
   var item = item_wrapper.find(settings.item_class);
@@ -20,25 +18,26 @@ $.fn.simpleRotator = function(options) {
   var items_margin = Math.round(rotator_count * (item_width/settings.items_visible));
   var busy = false;
 
-  if (rotator_count > items_visible) {
-    // Make the wrapper to contain all the items in one row.
+  if (rotator_count > settings.items_visible) {
+    // Make the wrapper to contain all the items in one row
     this.css({
       'width': rotator_count * settings.item_width,
       'height': item.outerHeight(true),
       'overflow': 'hidden',
+      'clear': 'both',
     });
-    
-    // Set the parent of the wrapper css so it doesn't show all items.
+
+    // Set the parent of the wrapper css so it doesn't show all items
     this.parent().css({
       'width': items_margin,
       'overflow': 'hidden',
     });
-    
+
     // Add slider controls
-    item_wrapper.before('<div class="slider-controls-'+settings.unique_id+'"><span class="nav go-left" data-dir="left">previous</span><span class="nav go-right" data-dir="right">next</span></div>');
+    item_wrapper.before('<div class="slider-controls slider-controls-'+settings.unique_id+'"><span class="navigation go-left" data-dir="left">previous</span><span class="navigation go-right" data-dir="right">next</span></div>');
   }
 
-  $(".slider-controls-"+settings.unique_id+" .nav").click(function(){
+  $(".slider-controls-"+settings.unique_id+" .navigation").click(function(){
     var dir = $(this).attr("data-dir"),
         limit = 1 - rotator_count/settings.items_visible;
 
@@ -48,7 +47,7 @@ $.fn.simpleRotator = function(options) {
         busy = true;
         item_wrapper.animate({
           'margin-left': "+="+items_margin
-        }, 1000);
+        }, 600);
         rotator_pos++;
         busy = false;
         //console.log("rotator_pos: "+rotator_pos);
@@ -58,7 +57,7 @@ $.fn.simpleRotator = function(options) {
         busy = true;
         item_wrapper.animate({
           'margin-left': "-="+items_margin
-        }, 1000);
+        }, 600);
         rotator_pos--;
         busy = false;
         //console.log("rotator_pos: "+rotator_pos);
